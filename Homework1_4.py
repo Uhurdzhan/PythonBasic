72121fe0c40b00cd9d187ecc42299bb73c03bf46
User <kotik.enotik2002@gmail.com>
2022-12-07 04:27:24 +0200

    The forth task

---
+++ Homework1_4.py
@@ -0,0 +1,12 @@
+"""letter_U = '\n***\t\t***\n *\t\t *\n *\t\t *\n *\t\t *\n *\t\t *\n *\t\t *\n  * * * *'
+letter_H = '\n***\t\t***\n *\t\t *\n *\t\t *\n * * * * *\n *\t\t *\n *\t\t *\n***\t\t***'
+letter_R = '\n* * * * *\n *\t\t *\n * * * *\n *\t  *\n *\t   *\n *\t\t*\n *\t\t *'
+print(letter_U, letter_H, letter_U, letter_R)"""
+first_string = '***\t\t*** ***\t\t*** ***\t\t*** * * * * *'
+second_string = '\n *\t\t *\t *\t\t *\t *\t\t *\t *\t\t *'
+third_string = '\n *\t\t *\t *\t\t *\t *\t\t *\t * * * *'
+forth_string = '\n *\t\t *\t * * * * *\t *\t\t *\t *\t  *'
+fifth_string = '\n *\t\t *\t *\t\t *\t *\t\t *\t *\t   *'
+sixth_string = '\n *\t\t *\t *\t\t *\t *\t\t *\t *\t\t*'
+seventh_string = '\n  * * * *\t***\t\t***\t  * * * *\t *\t\t *'
+print(first_string, second_string, third_string, forth_string, fifth_string, sixth_string, seventh_string)
