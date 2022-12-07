6e25690eee1d49e1d0bf3c6977b034da98bf745f
User <kotik.enotik2002@gmail.com>
2022-12-07 02:53:23 +0200

    The fifth task

---
+++ Homework1_5.py
@@ -0,0 +1,11 @@
+escape_sequences = '''
+Escape sequences
+\\a\t\tBell (alert)
+\\b\t\tBackspace
+\\n\t\tNew line
+\\t\t\tHorizontal tab
+\\\ \t\tBackslash \\
+\\"\t\tDouble quotation mark "
+\\'\t\tSingle quotation mark '
+'''
+print(escape_sequences)
