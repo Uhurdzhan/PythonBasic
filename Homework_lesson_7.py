db6b482d99e596500b4c9e142e6584fed834fd86
User <kotik.enotik2002@gmail.com>
2022-12-29 01:04:42 +0200

    done

---
+++ Homework_lesson_7.py
@@ -0,0 +1,33 @@
+"""# Task 3
+
+my_list_1 = []
+
+n = int(input("Enter number of elements for first list: "))
+for i in range(0, n):
+    user_input = int(input())
+    my_list_1.append(user_input)
+
+my_list_2 = []
+n = int(input("Enter number of elements for second list: "))
+for i in range(0, n):
+    user_input = int(input())
+    my_list_2.append(user_input)
+
+my_result = []
+for num in my_list_1:
+    if my_list_1.index(num) % 2 == 0:
+        my_result.append(num)
+
+for num in my_list_2:
+    if my_list_2.index(num) % 2 != 0:
+        my_result.append(num)
+print(my_result)"""
+
+# Task 11
+
+my_str = input("enter string: ")
+my_str_1 = []
+for i in my_str:
+    if my_str.count(i) == 1:
+        my_str_1.append(i)
+print(my_str_1)
